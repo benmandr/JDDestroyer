7ac8ab7485361cfccfc478eb978a203bd059daee
Vilius <vilius@rinkodara.lt>
2019-09-14 19:31:29 +0300

    Models init

---
+++ GameClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GameServer.Models;
+
+namespace GameClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
