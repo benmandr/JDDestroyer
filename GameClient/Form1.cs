4bf65550eae4ceade388ea96815ba60bbc65b34a
benmandr <bmandravickas@gmail.com>
2019-09-22 21:33:02 +0300

    Client one player example gui

--- GameClient/Form1.cs
+++ GameClient/Form1.cs
@@ -13,17 +13,22 @@ namespace GameClient
     public partial class Form1 : Form
     {
         Bitmap BackBuffer;
-        const int BallAxisSpeed = 2;
+        Bitmap FrontBuffer;
+        //Score management
+        int bestScore = 0; //SERVER VAR
+        string name = "Lohas"; //SERVER VAR
+        string scoreTextBox;
+        //Player
+        Point PlayerPos;
+        int playerSpeed = 10;
+        int PlayerSize;
 
-        Point BallPos = new Point(30, 30);
-        Point BallSpeed = new Point(BallAxisSpeed, BallAxisSpeed);
-        const int BallSize = 50;
 
         public Form1()
         {
             InitializeComponent();
             SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
-            //Go full screen with fixed square size
+           // Go full screen with fixed square size
             Text = "JDDestroyer";
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
@@ -31,79 +36,96 @@ namespace GameClient
             StartPosition = FormStartPosition.CenterScreen;
             int ScreenHeight = (int)(Screen.PrimaryScreen.Bounds.Height - (Screen.PrimaryScreen.Bounds.Height * 0.1));
             ClientSize = new Size(ScreenHeight, ScreenHeight);
+            //Set up the updating
             Timer GameTimer = new Timer();
             GameTimer.Interval = 10;
             GameTimer.Tick += new EventHandler(Tick);
             GameTimer.Start();
-
-            ResizeEnd += new EventHandler(CreateBackBuffer);
-            Load += new EventHandler(CreateBackBuffer);
-            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            Load += new EventHandler(Init);
             Paint += new PaintEventHandler(FormPaint);
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
         }
 
+        //Controls
         void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
-                BallSpeed.X = -BallAxisSpeed;
-            else if (e.KeyCode == Keys.Right)
-                BallSpeed.X = BallAxisSpeed;
+            Console.WriteLine("Player POS" + PlayerPos.X);
+            Console.WriteLine("Barrier:" + ClientSize.Width * 0.25);
+            if (e.KeyCode == Keys.Left && PlayerPos.X > ClientSize.Width * 0.25)
+                PlayerPos.X -= 1 * playerSpeed;
+            else if (e.KeyCode == Keys.Right && PlayerPos.X + PlayerSize < ClientSize.Width * 0.75)
+                PlayerPos.X += 1 * playerSpeed;
             else if (e.KeyCode == Keys.Up)
-                BallSpeed.Y = -BallAxisSpeed; // Y axis is downwards so -ve is up.
-            else if (e.KeyCode == Keys.Down)
-                BallSpeed.Y = BallAxisSpeed;
+                bestScore += 1;
         }
 
-        //Draw the main canvas without any moving objects
+        //Update paint
         void FormPaint(object sender, PaintEventArgs e)
         {
             if(BackBuffer != null)
             {
-                int length = ClientSize.Width;
-                //Main canvas
                 e.Graphics.DrawImageUnscaled(BackBuffer, Point.Empty);
-                //Main canvas color
-                e.Graphics.FillRectangle(new SolidBrush(Color.WhiteSmoke), new Rectangle(0, 0, length, length));
-                //Middle square
-                e.Graphics.DrawRectangle(new Pen(Color.DarkSlateGray), new Rectangle(length/4, length/4, length/2, length/2));
-                //Corner squares
-                SolidBrush cornerBrush = new SolidBrush(Color.Black);
-                int cornerCord = (int)(length * 0.75);
-                e.Graphics.FillRectangle(cornerBrush, new Rectangle(0,0, ClientSize.Width / 4, ClientSize.Height / 4));
-                e.Graphics.FillRectangle(cornerBrush, new Rectangle(0, cornerCord, ClientSize.Width / 4, ClientSize.Height / 4));
-                e.Graphics.FillRectangle(cornerBrush, new Rectangle(cornerCord, 0, ClientSize.Width / 4, ClientSize.Height / 4));
-                e.Graphics.FillRectangle(cornerBrush, new Rectangle(cornerCord, cornerCord, ClientSize.Width / 4, ClientSize.Height / 4));
-               
+                e.Graphics.DrawImageUnscaled(FrontBuffer, Point.Empty);
             }
         }
 
-        void CreateBackBuffer(object sender, EventArgs e)
+        void Init(object sender, EventArgs ev)
         {
             if (BackBuffer != null)
+            {
                 BackBuffer.Dispose();
-            BackBuffer = new Bitmap(Width,Height);
+                FrontBuffer.Dispose();
+            }
+            if (PlayerPos != null)
+            {
+                PlayerPos = new Point(0,0);
+            }
+            BackBuffer = new Bitmap(ClientSize.Width,ClientSize.Height);
+            FrontBuffer = new Bitmap(ClientSize.Width, ClientSize.Height);
+            PlayerPos = new Point((int)(ClientSize.Width * 0.26), ClientSize.Height);
+            PlayerSize = ClientSize.Width / 8;
+            using (var e = Graphics.FromImage(BackBuffer))
+            {
+                int length = ClientSize.Width;
+                //Main canvas color
+                e.FillRectangle(Brushes.WhiteSmoke, new Rectangle(0, 0, length, length));
+                //Middle square
+                Rectangle middleSquare = new Rectangle(length / 4, length / 4, length / 2, length / 2);
+                e.FillRectangle(Brushes.Gray, middleSquare);
+                //Add border
+                Pen borderPen = new Pen(Color.FromArgb(105, 105, 105), 4);
+                borderPen.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+                e.DrawRectangle(borderPen, middleSquare);
+                //Corner squares
+                int cornerCord = (int)(length * 0.75);
+                e.FillRectangle(Brushes.Black, new Rectangle(0, 0, length / 4, length / 4));
+                e.FillRectangle(Brushes.Black, new Rectangle(0, cornerCord, length / 4, length / 4));
+                e.FillRectangle(Brushes.Black, new Rectangle(cornerCord, 0, length / 4, length / 4));
+                e.FillRectangle(Brushes.Black, new Rectangle(cornerCord, cornerCord, length / 4, length / 4));
+                //Score title
+                e.DrawString("Best score:", new Font("Comic Sans MS", 18), Brushes.White, (float)(length * 0.75), 0);
+            }
         }
 
         void Draw()
         {
-            if(BackBuffer != null)
+            if (BackBuffer != null)
             {
-                using (var graphic = Graphics.FromImage(BackBuffer))
+                using (var graphic = Graphics.FromImage(FrontBuffer))
                 {
-                    //InstantiateMainBlock(graphic);
-                    graphic.Clear(Color.White);
-                    //graphic.FillEllipse(Brushes.Black, BallPos.X - BallSize / 2, BallPos.Y - BallSize / 2, BallSize, BallSize);
-                   
+                    graphic.Clear(Color.Transparent);
+                    //Update the score
+                    graphic.DrawString(scoreTextBox, new Font("Comic Sans MS", 18), Brushes.White, (float)(ClientSize.Width * 0.75), 28);
+                    graphic.FillRectangle(Brushes.Green, PlayerPos.X, PlayerPos.Y - PlayerSize, PlayerSize, PlayerSize);
+                    graphic.DrawString("P1", new Font("Comic Sans MS", 30), Brushes.White, (int)(PlayerPos.X + 20), PlayerPos.Y - 70);
                 }
-
                 Invalidate();
             }
         }
 
         void Tick(object sender, EventArgs e)
         {
-            //BallPos.X += BallSpeed.X;
-            //BallPos.Y += BallSpeed.Y;
+            scoreTextBox = name + bestScore.ToString();
             Draw();
         }
     }
