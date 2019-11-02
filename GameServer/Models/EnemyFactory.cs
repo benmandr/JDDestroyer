f913c52d7797cd5974cb35c8e7c658647958f2ab
benmandr <bmandravickas@gmail.com>
2019-11-02 16:57:57 +0200

    enemy oclinameble

--- GameServer/Models/EnemyFactory.cs
+++ GameServer/Models/EnemyFactory.cs
@@ -9,7 +9,7 @@ namespace GameServer.Models
     class EnemyFactory
     {
         private static EnemyFactory instance = new EnemyFactory();
-        private static Map<int, Enemy> enemyMap
+        private static Dictionary<int, Enemy> enemyMap = new Dictionary<int, Enemy>();
         public static EnemyFactory getInstance()
         {
             return instance;
@@ -18,7 +18,7 @@ namespace GameServer.Models
         private static Position getRandomInMiddle() {
             Random random = new Random();
             double min = Config.CORNERSIZE + Config.ENEMYSIZE / 2;
-            double max = Config.INNERSQUARESIZE + Config.CORNERSIZE - Config.ENEMYSIZE/2;
+            double max = Config.INNERSQUARESIZE + Config.CORNERSIZE - Config.ENEMYSIZE / 2;
 
 
             double x = random.NextDouble() * (max - min) + min;
@@ -40,6 +40,19 @@ namespace GameServer.Models
                     return new BlueEnemy(position);  //@todo
             }
             return null;
+        }
+
+        private void addEnemies()
+        {
+            Console.WriteLine("Started creating enemies");
+            Console.WriteLine("Enemy type red: " + RedEnemy.TYPE);
+            enemyMap.Add(RedEnemy.TYPE, new RedEnemy(getRandomInMiddle()));
+            Console.WriteLine("Enemy type green: " + GreenEnemy.TYPE);
+
+            enemyMap.Add(GreenEnemy.TYPE, new GreenEnemy(getRandomInMiddle()));
+            Console.WriteLine("Enemy type blue: " + BlueEnemy.TYPE);
+
+            enemyMap.Add(BlueEnemy.TYPE, new BlueEnemy(getRandomInMiddle()));
         }
     }
 }
